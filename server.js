a8ab9bb4d7f6641f364cf6201c3753a3107e5d3d
ngocdiepvu1 <ngocdiep240802@gmail.com>
2024-05-31 14:50:40 -0400

    pages

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // Server side
 const express = require('express');
+const path = require('path'); // Require the path module
 const { readFile } = require('fs');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
-  readFile('./public/index.html', 'utf8', (err, html) => {
+  readFile(path.join(__dirname, 'index.html'), 'utf8', (err, html) => {
     if (err) {
       res.status(500).send('Sorry, out of order');
+      return;
     }
     res.send(html);
   });
